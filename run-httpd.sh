8032d8ef331347d1b524fa91bc2d12b9e391e2fc
reljicb <bojan.reljic@gmail.com>
2020-01-28 11:08:09 +0100

    fix: make SSL generic

--- run-httpd.sh
+++ run-httpd.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 docker run --rm -dit \
-	--name=httpd \
-	-p 8443:8443 \
-	-v $(pwd):/usr/local/apache2/htdocs \
-	-v $(pwd)/httpd.conf:/usr/local/apache2/conf/httpd.conf \
-	-v $(pwd)/httpd-vhosts.conf:/usr/local/apache2/conf/extra/httpd-vhosts.conf \
-	-v $(pwd)/ssl/:/usr/local/apache2/conf/ssl/ \
-	httpd:2.4
+        --name=httpd \
+        -p 443:443 \
+        -v $(pwd):/usr/local/apache2/htdocs \
+        -v $(pwd)/httpd.conf:/usr/local/apache2/conf/httpd.conf \
+        -v $(pwd)/httpd-vhosts.conf:/usr/local/apache2/conf/extra/httpd-vhosts.conf \
+        -v $(pwd)/ssl/proxy-1.certificate.pem:"/usr/local/apache2/conf/ssl/proxy.certificate.pem" \
+        -v $(pwd)/ssl/proxy-1.key.pem:"/usr/local/apache2/conf/ssl/proxy.key.pem" \
+        -v $(pwd)/ssl/cacerts.pem:"/usr/local/apache2/conf/ssl/cacerts.pem" \
+        httpd:2.4
